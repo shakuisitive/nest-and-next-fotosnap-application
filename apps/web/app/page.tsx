3e73e0441f32677dd1d81f4bdb898418eda29e50
Shakir <shakirkhan72727@gmail.com>
2026-04-18 02:31:55 +0500

    Set up Shadcn

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,5 @@
 export default function HomePage() {
-  return <h1>Hello World</h1>
+  return <div className="bg-gray-100">
+    <h1 className="text-3xl font-bold tracking-tighter p-10">Hello World</h1>
+  </div>
 }
